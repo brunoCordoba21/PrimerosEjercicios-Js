550937ae746a050807fbd00b681d41f95d2e6eec
Bruno Cordoba <cordobabruno32@gmail.com>
2024-07-02 19:15:33 -0300

    se realizo el ejercicio 5 del tp1 Js, y se creo los documentos js para los demas ejercicios

---
+++ js/05-ejercicio5.js
@@ -0,0 +1,11 @@
+/*5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
+*/
+
+const numero1 = parseInt(prompt ("ingrese un numero"))
+console.log (numero1)
+const numero2 = parseInt (prompt("ingrese un numero"))
+console.log(numero2)
+const resultado = (numero1 + numero2) ;
+console.log (resultado)
